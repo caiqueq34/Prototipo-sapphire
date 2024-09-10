aea8056835e6181447f58798caecc1bb19757da2
MMaranini <manuela.maranini@gmail.com>
2024-09-10 17:29:16 -0300

    Arrumando header

--- script.js
+++ script.js
@@ -7,10 +7,13 @@ function abrirNavbar()
   if(mostraNavbar){
     navheader.style.marginRight ='-60vw';
     navheader.style.animationName = 'mostraNavbar';
+    navheader.style.display = 'block';
+    navheader.style.position = 'fixed';
   }
   else{
     navheader.style.marginRight='-140vw';
     navheader.style.animationName = '';
+    navheader.style.display = 'none';
   }
 }
 window.addEventListener('resize', function(event) {
