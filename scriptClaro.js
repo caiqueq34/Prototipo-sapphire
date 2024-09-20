186d8a15daac66ac53b33e586eb9298139f40eae
MMaranini <manuela.maranini@gmail.com>
2024-09-20 14:20:04 -0300

    Adicionando modo claro

---
+++ scriptClaro.js
@@ -0,0 +1,24 @@
+var navheader = document.getElementById('nav-header');
+var mostraNavbar = false;
+
+function abrirNavbar()
+{
+  mostraNavbar = !mostraNavbar;
+  if(mostraNavbar){
+    navheader.style.marginRight ='-60vw';
+    navheader.style.animationName = 'mostraNavbar';
+    navheader.style.display = 'flex';
+    navheader.style.position = 'fixed';
+  }
+  else{
+    navheader.style.marginRight='-140vw';
+    navheader.style.animationName = '';
+    navheader.style.display = 'none';
+  }
+}
+window.addEventListener('resize', function(event) {
+  if(window.innerWidth > 768 && mostraNavbar)
+  {
+    abrirNavbar();
+  }
+});
