3f07ac3a4c390a7b546d37d8e4fbe9c673830b77
MMaranini <manuela.maranini@gmail.com>
2024-10-11 18:52:06 -0300

    upgrade carrossel logos

--- script.js
+++ script.js
@@ -22,3 +22,29 @@ window.addEventListener('resize', function(event) {
     abrirNavbar();
   }
 });
+
+//fazendo lógica do carrossel
+const carrossel = document.querySelector(".swipper-wrapper");
+
+let isDragStart = false, prevPageX, prevScrollLeft;
+
+const dragStart = (e) => {
+  isDragStart = true;
+  prevPageX = e.pageX;
+  prevScrollLeft = carrossel.scrollLeft;
+}
+
+const dragging = (e) => {
+  if(!isDragStart) return;
+  e.preventDefault();
+  let positionDiff = e.pageX - prevPageX;
+  carrossel.scrollLeft = prevScrollLeft - positionDiff;
+}
+
+const dragStop = () => {
+  isDragStart = false;
+}
+
+carrossel.addEventListener("mousedown", dragStart);
+carrossel.addEventListener("mousemove", dragging);
+carrossel.addEventListener("mouseup", dragStop);
